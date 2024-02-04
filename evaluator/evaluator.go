f6a85fee04099452f80e2fbe8611204fa8d6c1e4
titivuk <titivuk@gmail.com>
2024-02-04 20:14:19 +0100

    3.5 - evaluating infix expressions

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -26,8 +26,12 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(left, node.Operator, right)
 	default:
-		return nil
+		return NULL
 	}
 }
 
@@ -61,6 +65,55 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 
 		value := right.(*object.Integer).Value
 		return &object.Integer{Value: -value}
+	default:
+		return NULL
+	}
+}
+
+func evalInfixExpression(left object.Object, operator string, right object.Object) object.Object {
+	switch {
+	case right.Type() == object.INTEGER_OBJ && left.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(left, operator, right)
+	case right.Type() == object.BOOLEAN_OBJ && left.Type() == object.BOOLEAN_OBJ:
+		return evalBooleanInfixExpression(left, operator, right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(left object.Object, operator string, right object.Object) object.Object {
+	leftValue := left.(*object.Integer).Value
+	rightValue := right.(*object.Integer).Value
+
+	switch operator {
+	case token.PLUS:
+		return &object.Integer{Value: leftValue + rightValue}
+	case token.MINUS:
+		return &object.Integer{Value: leftValue - rightValue}
+	case token.ASTERISK:
+		return &object.Integer{Value: leftValue * rightValue}
+	case token.SLASH:
+		return &object.Integer{Value: leftValue / rightValue}
+	case token.LT:
+		return nativeBoolToBooleanObject(leftValue < rightValue)
+	case token.GT:
+		return nativeBoolToBooleanObject(leftValue > rightValue)
+	case token.EQ:
+		return nativeBoolToBooleanObject(leftValue == rightValue)
+	case token.NOT_EQ:
+		return nativeBoolToBooleanObject(leftValue != rightValue)
+	default:
+		return NULL
+	}
+}
+
+func evalBooleanInfixExpression(left object.Object, operator string, right object.Object) object.Object {
+	// we can use pointer comparison because we always use TRUE and FALSE objects
+	switch operator {
+	case token.EQ:
+		return nativeBoolToBooleanObject(left == right)
+	case token.NOT_EQ:
+		return nativeBoolToBooleanObject(left != right)
 	default:
 		return NULL
 	}
